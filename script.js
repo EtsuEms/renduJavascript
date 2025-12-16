e251bad59b88d3d81075757813ee20e662d6007f
Mehdi <elmehdi.ettsouli@latoile.em-lyon.com>
2025-12-16 12:29:50 +0100

    commit no.3
    section avantage : en cours

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ fetch(jsonUrl)
    // 2. Récuperer les éléments DOM 
     const main = document.getElementById("escapeGame")
 
+    const logo = document.getElementById("logoNav");
     const heroContainer = document.getElementById("heroSection");
     const avantageContainer = document.getElementById("avantagesSection");
     const activityContainer = document.getElementById("activitySection");
     const temoignagesContainer = document.getElementById("temoignagesSection");
+
+    logo.textContent = data.nomCommercial;
 
     // 3. création élément section héro
     const phraseAccroche = document.createElement("h1");
@@ -31,12 +34,24 @@ fetch(jsonUrl)
     heroContainer.appendChild(btnCTA);  
     
 
-    // 4. création éléments section avantages
+    // création éléments section avantages
+    array.forEach(avantage => {
+        afficherAvantage(avantage);
+    });    
 
-    
 
-    // 5. création éléments section activité
 
-    // 6. création éléments section témoignages
+    // création éléments section activité
+
+    // création éléments section témoignages
 
 });
+
+
+// fonction carte avantage
+function afficherAvantage(avantage) {
+    const div = document.createElement("div");
+
+    const avantageTitle = document.createElement("h3");
+    avantageTitle.textContent = 
+}
